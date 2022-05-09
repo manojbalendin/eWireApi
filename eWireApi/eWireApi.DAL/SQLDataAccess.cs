2b3d188a6316a5b63813fbfb2adbbc4b85952a13
manoj.singh <manoj.singh@pearson.com>
2022-05-09 20:20:13 +0530

    SqlDataAccess with transaction added

---
+++ eWireApi/eWireApi.DAL/SQLDataAccess.cs
@@ -0,0 +1,60 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Data.SqlClient;
+using System;
+
+namespace TempleManagement_API.DAL
+{
+    public class SQLDataAccess : IDisposable
+    {
+        protected readonly IConfiguration _config;
+        public SQLDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        private IDbConnection _dbConnection;
+        private IDbTransaction _dbTransaction;
+        public void StartTransaction()
+        {
+            _dbConnection = this.Connection;
+            _dbTransaction=_dbConnection.BeginTransaction();
+            _dbConnection.Open();
+        }
+
+        public void SaveDataInTransaction<T>(string storeProcedure, T Parameters)
+        {
+            _dbConnection.Execute(storeProcedure, Parameters, commandType: CommandType.StoredProcedure, transaction: _dbTransaction);
+        }
+
+        public List<T> LoadDataInTransaction<T, U>(string storeProcedure, U parameters)
+        {
+            List<T> rows= _dbConnection.Query<T>(storeProcedure, parameters,commandType:CommandType.StoredProcedure,transaction:_dbTransaction).ToList();
+            return rows;
+        }
+        public void CommitTransaction()
+        {
+            _dbTransaction?.Rollback();
+            _dbConnection?.Close();
+        }
+        public void RollbackTransaction()
+        {
+            _dbTransaction?.Rollback();
+            _dbConnection?.Close();
+        }
+        public void Dispose()
+        {
+            CommitTransaction();
+        }
+        
+    }
+}
